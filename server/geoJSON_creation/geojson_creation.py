28955d3564f87afa6db1e0745929286492a1d38e
Lorenza Pasquini <lorenza.pasquini@hotmail.it>
2020-12-04 18:47:39 +0100

    added lists stringify in the geojson files

--- server/geoJSON_creation/geojson_creation.py
+++ server/geoJSON_creation/geojson_creation.py
@@ -1,4 +1,5 @@
 from osgeo import ogr
+from geojson import FeatureCollection, Feature, Point, Polygon
 
 def object_list_to_geojson_file(filename, object_list):
     if object_list:
@@ -26,11 +27,32 @@ def object_list_to_geojson_file(filename, object_list):
             # set fields
             for attribute in dir(object):
                 if attribute[0:3] == 'get' and attribute[4:len(attribute)] != 'geometry': #take all the attributes (different from the geometry) by using the methods starting with "get"
-                    outFeature.SetField(attribute[4:len(attribute)], str(getattr(object, attribute)()))
+                    result = getattr(object, attribute)()
+                    attr_value = ''
+                    if type(result) == list:
+                        attr_value = attr_value+'['
+                        listToString = ', '.join([str(elem) for elem in result])
+                        attr_value = attr_value+listToString+']'
+                    else:
+                        attr_value =str(result)
+                    outFeature.SetField(attribute[4:len(attribute)], attr_value)
             outLayer.CreateFeature(outFeature)
         outFeature = None
         outDataSource = None
     else:
         print ('geojson file <'+filename+'> not created: there are no element in the list')
 
-
+'''
+def object_list_to_geojson(filename, object_list):
+    features = []
+    for object in object_list:
+        geom = object.get_geometry()
+        if (geom.GetGeometryType == 'POLYGON'):
+        Feature(Polygon())
+        print(wtk_geom)
+        feature = Feature(POLYGON ((13.185225 42.519176,13.397271 42.378363,13.326091 42.319939,13.113885 42.460752,13.185225 42.519176)))
+        feature = Feature(geometry = wtk_geom)
+        features.append(feature)
+    feature_collection = FeatureCollection(features)
+    print(feature_collection)
+'''
